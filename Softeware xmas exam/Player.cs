766b65980f0a00e9a2f37570f20e95fe37541ad8
James Bishop <S00187350@mail.itsligo.ie>
2019-12-11 09:47:36 +0000

    added the props

---
+++ Softeware xmas exam/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Softeware_xmas_exam
+{
+
+    public enum Position {Goalkeeper, Defender, Midfilder, Forward }
+
+    class Player
+    {
+
+        //props
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public Position PreferredPossition { get; set; }
+        public DateTime DoB { get; set; }
+
+        private int _age;
+        public int Age
+        {
+            get
+            {
+                _age = DateTime.Now.Year - DoB.Year;
+                if (DoB.DayOfYear > DateTime.Now.DayOfYear)
+                    _age--;
+                return _age;
+            }
+        }
+
+    }
+}
